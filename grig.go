908798f384faf2040410903d68d273ea45cdc68b
Claes Mogren <claes.mogren@gmail.com>
2014-01-23 21:07:44 +0100

    Added list flag

--- grig.go
+++ grig.go
@@ -3,15 +3,35 @@ package main
 import (
 	"flag"
 	"fmt"
+  "os"
+  "io/ioutil"
 )
 
 var langFlag string
+var listLangFlag bool
 
 func init() {
-	flag.StringVar(&langFlag, "lang", "en", "ISO lang code")
+	flag.StringVar(&langFlag, "lang", "en", "Select ISO 639-1 language code")
+	flag.BoolVar(&listLangFlag, "l", false, "List available ISO language codes")
 }
 
 func main() {
 	flag.Parse()
+  if (listLangFlag) {
+    fmt.Println("list: ")
+    return
+  }
 	fmt.Println("hello " + langFlag)
+  loadData(langFlag)
 }
+
+func loadData(iso string) {
+  file, err := ioutil.ReadFile("data/" + langFlag + "/fnames.grig")
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(0)
+  }
+  str := string(file)
+  fmt.Println(str)
+}
+
